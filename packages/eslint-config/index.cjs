001758896dc1b9ca034a7e8f8ad1f899708912ba
oumer922 <oumerhussein92@gmail.com>
2025-09-23 00:54:07 +0300

    chore(config): add shared TypeScript & ESLint configs

---
+++ packages/eslint-config/index.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "react", "react-hooks"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:react/jsx-runtime",
+    "prettier"
+  ],
+  settings: { react: { version: "detect" } }
+};
